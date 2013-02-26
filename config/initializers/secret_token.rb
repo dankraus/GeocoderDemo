3e3e07c4e71f68aa7e5cd95664c68b0047c23a43
Dan Kraus <dskraus@gmail.com>
2013-02-26 14:42:40 -0600

    First commit. Includes GeoCoder gem and RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeocoderDemo::Application.config.secret_token = 'b1da8c546fc15455d4b41d4dbbcefd1600db89cca41e5bae058d849d63361920079d073e5806b317018594ab8dfca50a50b8d3f820cb2310019c387047ecc2d6'
